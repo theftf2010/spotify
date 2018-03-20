8c524e94e5e60215b2fe14d7953f7c9dc68c9028
theftf2010 <36692435+theftf2010@users.noreply.github.com>
2018-03-20 09:59:03 +0100

    Webservice fertig

--- src/main/java/dhbw/webservice/SearchWebservice.java
+++ src/main/java/dhbw/webservice/SearchWebservice.java
@@ -5,32 +5,97 @@
  */
 package dhbw.webservice;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dhbw.pojo.result.search.SearchResult;
+import dhbw.pojo.result.search.SearchResultList;
+import dhbw.pojo.search.album.SearchAlbum;
+import dhbw.pojo.search.artist.SearchArtist;
+import dhbw.pojo.search.track.SearchTrack;
+import dhbw.spotify.RequestCategory;
+import dhbw.spotify.RequestType;
 import dhbw.spotify.SpotifyRequest;
-
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Foecking
  */
-
-@Path("/search")
+@RestController
 public class SearchWebservice {
-    
-  @GET
-  @Produces( MediaType.APPLICATION_JSON )
-  @Path("query/{query}/type/{type}")
-  public String searchResultJson( @PathParam("query") String query,
-                                  @PathParam("type") String type
-  )
-          
-  {
-      
-      
-    return null;
-  }
-    
+
+    @RequestMapping("/search")
+    public String searchResultJson(@RequestParam(value = "query") String query, @RequestParam(value = "type") String type) {
+        SpotifyRequest spotifyRequest = new SpotifyRequest(RequestType.SEARCH);
+        Optional<String> stringOptional;
+        RequestCategory category;
+        category = RequestCategory.valueOf(type);
+        String s = "";
+        String json = "";
+        try {
+            stringOptional = spotifyRequest.performeRequestSearch(category, query, 10, "DE");
+            if (stringOptional.isPresent()) {
+                s = stringOptional.get();
+            }
+            ObjectMapper om = new ObjectMapper();
+            List<dhbw.pojo.search.track.Item> itemTrack = new ArrayList<>();
+            List<dhbw.pojo.search.album.Item> itemAlbum = new ArrayList<>();
+            List<dhbw.pojo.search.artist.Item> itemArtist = new ArrayList<>();
+            List<SearchResultList> resultList = new ArrayList<>();
+            switch (type) {
+                case "TRACK":
+                    SearchTrack track = om.readValue(s, SearchTrack.class);
+                    itemTrack = track.getTracks().getItems();
+                    for (dhbw.pojo.search.track.Item element : itemTrack) {
+                        String id = element.getId();
+                        String title = element.getName();
+                        String description = element.getType();
+                        String playLink = element.getUri();
+                        SearchResultList resultItemList = new SearchResultList(id, title, description, playLink);
+                        resultList.add(resultItemList);
+                    }
+                    break;
+                case "ALBUM":
+                    SearchAlbum album = om.readValue(s, SearchAlbum.class);
+                    itemAlbum = album.getAlbums().getItems();
+                    for (dhbw.pojo.search.album.Item element : itemAlbum) {
+                        String id = element.getId();
+                        String title = element.getName();
+                        String description = element.getType();
+                        String playLink = element.getUri();
+                        SearchResultList resultItemList = new SearchResultList(id, title, description, playLink);
+                        resultList.add(resultItemList);
+                    }
+                    break;
+                case "ARTIST":
+                    SearchArtist artist = om.readValue(s, SearchArtist.class);
+                    itemArtist = artist.getArtists().getItems();
+                    for (dhbw.pojo.search.artist.Item element : itemArtist) {
+                        String id = element.getId();
+                        String title = element.getName();
+                        String description = element.getType();
+                        String playLink = element.getUri();
+                        SearchResultList resultItemList = new SearchResultList(id, title, description, playLink);
+                        resultList.add(resultItemList);
+                    }
+                    break;
+                default:
+
+            }
+            SearchResult searchResult = new SearchResult();
+            searchResult.setSearchTerm(query);
+            searchResult.setSearchCategory(type);
+            searchResult.setResults(resultList);
+            json = om.writeValueAsString(searchResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
 }
+
 
